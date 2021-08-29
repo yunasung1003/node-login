6daeab84d389643e6a59a48aec5b0caa19eb8ed0
yunasung1003 <yuna.sung.1003@gmail.com>
2021-08-29 17:05:13 +0900

    파일정리

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const PORT = 3000;
 const home = require("./src/routes/home");
 
 //앱 셋팅
-app.set("views", "./src/views");
+app.set("views", "./src/src/views");
 app.set("view engine", "ejs");
 
 app.use("/", home); // use -> 미들웨어를 등록해주는 메소드.
