eaa6ad536edb9dec7df8c75c88e409c04f42da20
yunasung1003 <yuna.sung.1003@gmail.com>
2021-08-29 17:41:16 +0900

    .

--- src/app.js
+++ src/app.js
@@ -11,7 +11,9 @@ const home = require("./src/routes/home");
 //앱 셋팅
 app.set("views", "./src/src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`))
 
 app.use("/", home); // use -> 미들웨어를 등록해주는 메소드.
+
 
 module.exports = app;
