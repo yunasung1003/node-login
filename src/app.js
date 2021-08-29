1d1d15c857404159127906de7dc44f6c02bb163b
yunasung1003 <yuna.sung.1003@gmail.com>
2021-08-29 16:40:53 +0900

    .

--- src/app.js
+++ src/app.js
@@ -6,10 +6,10 @@ const app = express();
 const PORT = 3000;
 
 //라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 //앱 셋팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use("/", home); // use -> 미들웨어를 등록해주는 메소드.
