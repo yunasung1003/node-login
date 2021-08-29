28a0a92811dd6361871d3e2bea526327e36c01fc
yunasung1003 <yuna.sung.1003@gmail.com>
2021-08-29 22:51:33 +0900

    DOM으로 HTML 객체 저장

--- src/src/public/js/home/login.js
+++ src/src/public/js/home/login.js
@@ -1,5 +1,16 @@
 "use strict";
 
+const id = document.querySelector("#id"),
+psword = document.querySelector("#psword"),
+loginBtn = document.querySelector("button");
 
-console.log("hello");
-console.log("world");
+loginBtn.addEventListener("click", login);
+
+function login(){
+   const req = {
+       id : id.value,
+       psword : psword.value,
+   };
+   console.log(req);
+}
+
